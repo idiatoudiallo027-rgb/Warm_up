99b09bafdd4c3510dbbde9f49877bb362d4c39a9
idiatou <idiatoudiallo027@gmail.com>
2026-01-21 20:03:44 -0500

    exercice sur les nombres paires et impaires

---
+++ exercice4.py
@@ -0,0 +1,11 @@
+liste=[1,3,8,6,12,78,15,4,69,13]
+listepaire=[]
+listeimpaire=[]
+for i in liste:
+    if i%2==0:
+        listepaire.append(i)
+    else:
+        listeimpaire.append(i)
+print(listepaire)
+print(listeimpaire)
+
